43c0015ed02857921f937826f4025bbad7791597
Bruno Silva Santana <ibrunoss@outlook.com>
2024-03-31 16:24:18 -0300

    feat: add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <div className="flex justify-center w-full p-4 bg-gray-950">
+      <h1 className="text-xl text-gray-200">
+        Desafio de adicionar novo elemento ao clicar entre os elementos
+        existentes
+      </h1>
+    </div>
+  );
+}
+
+export default Header;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from "./Button";
 export { default as ConditionalRendering } from "./ConditionalRendering";
 export { default as Footer } from "./Footer";
+export { default as Header } from "./Header";
