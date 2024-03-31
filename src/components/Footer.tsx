370c4ae727ecfe915f5bdada73b49e3a1be77eaf
Bruno Silva Santana <ibrunoss@outlook.com>
2024-03-31 16:23:15 -0300

    feat: add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+function Footer() {
+  return (
+    <footer className="text-gray-500">
+      Desenvolvido por{" "}
+      <a
+        className="font-bold text-gray-400 hover:text-gray-200"
+        href="https://github.com/ibrunoss"
+        target="_blank"
+        referrerPolicy="no-referrer"
+      >
+        ibrunoss
+      </a>{" "}
+      com ReactJS e TailwindCSS
+    </footer>
+  );
+}
+
+export default Footer;
