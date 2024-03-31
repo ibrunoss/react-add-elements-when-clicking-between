8b0e48e5a359abe08e0ef3034842c32ad452195b
Bruno Silva Santana <ibrunoss@outlook.com>
2024-03-31 17:40:05 -0300

    feat: solves the challenge

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
-import React from "react";
-import InputRequirements from "./components/InputRequirements";
+import { Fragment } from "react";
+import {
+  Input,
+  Footer,
+  Header,
+  ConditionalRendering,
+  Button,
+} from "./components";
+import useCells from "./hooks/useCells";
 
 function App() {
-  const [input, setInput] = React.useState("");
-  const resetInput = () => setInput("");
-  /*    */
-
+  const { addCellAfterIndex, cells, cellsValueJoined, updateCellByIndex } =
+    useCells();
   return (
-    <div>
-      <button
-        type="button"
-        onClick={() => {
-          resetInput();
-        }}
-      >
-        Reset
-      </button>
-      <InputRequirements
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      />
+    <div className="flex flex-col items-center justify-between h-full min-h-screen text-gray-100 bg-gray-900">
+      <Header />
+      <main className="flex flex-col items-center gap-4">
+        <section className="flex items-center">
+          {cells.map((cell, idx) => (
+            <Fragment key={idx}>
+              <Input
+                value={cell}
+                onChange={(e) => updateCellByIndex(e.target.value, idx)}
+              />
+              <ConditionalRendering renderIf={idx < cells.length - 1}>
+                <Button onClick={() => addCellAfterIndex(idx)} />
+              </ConditionalRendering>
+            </Fragment>
+          ))}
+        </section>
+        <p>{cellsValueJoined}</p>
+      </main>
+      <Footer />
     </div>
   );
 }
